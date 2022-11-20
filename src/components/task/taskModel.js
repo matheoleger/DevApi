625ac0650dc938278564982f8eb945fcb6317eaf
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-11-20 22:07:15 +0100

    chore(taskModel.js): I removed useless functions

--- src/components/task/taskModel.js
+++ src/components/task/taskModel.js
@@ -18,26 +18,4 @@ const taskSchema = new Schema({
 
 const Task = mongoose.model('Task', taskSchema)
 
-const findAll = async () => {
-    const tasks = await Task.find()
-    console.log('FIND ALL ================', tasks);
-}
-
-const findById = async () => {
-    const task = await Task.findById('637219c10a1d76792aa53b81')
-    console.log(task);
-}
-
-const updateById = async () => {
-    const task = await Task.findByIdAndUpdate('637219c10a1d76792aa53b81', {
-        name: 'Premier élément modifié'
-    },{
-        runValidators: true
-    })
-}
-
-const deleteById = async () => {
-    const task = await Task.findByIdAndDelete('637219c10a1d76792aa53b81')
-}
-
 export default Task;
