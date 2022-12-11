389eb7fb8fc2f7c6b4a7b753255a7bf83167fb0c
matheoleger <71370253+matheoleger@users.noreply.github.com>
2022-12-11 18:15:06 +0100

    feat(list, task): I modify functions to get/update/delete/create tasks and lists when we are login.
    I added user property in tasks and lists to verify if a list or a task belongs to user.

--- src/components/list/listRoute.js
+++ src/components/list/listRoute.js
@@ -1,12 +1,13 @@
 import Router from "@koa/router"
 import * as ListControllers from "#components/list/listControllers.js"
+import {isAuthenticated,isAuthenticatedWithUser} from "#middlewares/jwtHandler.js"
 
 const lists = new Router()
 
-lists.get('/lists', ListControllers.getLists)
-lists.get('/lists/:id', (ctx) => ListControllers.getListById(ctx))
-lists.delete('/lists/:id', ListControllers.deleteById)
-lists.put('/lists/:id', ListControllers.updateById)
-lists.post('/lists', (ctx)=>ListControllers.create(ctx))
+lists.get('/lists', isAuthenticatedWithUser, ListControllers.getLists)
+lists.get('/lists/:id', isAuthenticatedWithUser, (ctx) => ListControllers.getListById(ctx))
+lists.delete('/lists/:id', isAuthenticatedWithUser, ListControllers.deleteById)
+lists.put('/lists/:id', isAuthenticatedWithUser, ListControllers.updateById)
+lists.post('/lists', isAuthenticatedWithUser, (ctx)=>ListControllers.create(ctx))
 
 export default lists
--- src/components/task/taskRoute.js
+++ src/components/task/taskRoute.js
@@ -4,16 +4,16 @@ import {isAuthenticated,isAuthenticatedWithUser} from "#middlewares/jwtHandler.j
 
 const tasks = new Router()
 
-tasks.get('/tasks', TaskControllers.getTasks)
-tasks.get('/tasks/protected', isAuthenticatedWithUser, (ctx) => {
-    ctx.ok({
-        message: "This route is protected",
-        user: ctx.state.user
-    })
-})
-tasks.get('/tasks/:id', (ctx) => TaskControllers.getTaskById(ctx))
-tasks.delete('/tasks/:id', TaskControllers.deleteById)
-tasks.put('/tasks/:id', TaskControllers.updateById)
-tasks.post('/tasks', (ctx)=>TaskControllers.create(ctx))
+tasks.get('/tasks', isAuthenticatedWithUser, TaskControllers.getTasks)
+// tasks.get('/tasks/protected', isAuthenticatedWithUser, (ctx) => {
+//     ctx.ok({
+//         message: "This route is protected",
+//         user: ctx.state.user
+//     })
+// })
+tasks.get('/tasks/:id', isAuthenticatedWithUser, (ctx) => TaskControllers.getTaskById(ctx))
+tasks.delete('/tasks/:id', isAuthenticatedWithUser, TaskControllers.deleteById)
+tasks.put('/tasks/:id', isAuthenticatedWithUser, TaskControllers.updateById)
+tasks.post('/tasks', isAuthenticatedWithUser, (ctx)=>TaskControllers.create(ctx))
 
 export default tasks
