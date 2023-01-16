de47825a8d5c09dccf2650bca1a22e487c9a7bdc
matheoleger <71370253+matheoleger@users.noreply.github.com>
2023-01-16 12:06:27 +0100

    fix(indexPage): I fixed button getStarted

--- front/src/pages/IndexPage.vue
+++ front/src/pages/IndexPage.vue
@@ -3,23 +3,19 @@
     <div>
       <h5>Ynov Tasks Manager</h5>
       <p>Try out this new lightweight tasks manager for everyday tasks</p>
-      <q-btn no-caps @click="redirectDashboard">Get started</q-btn>
+      <q-btn no-caps @click="redirectDashboard" label="Get Started"/>
     </div>
     <img v-bind:src="'src/assets/header-img.png'"/>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
+<script setup>
 import { useRouter, useRoute } from 'vue-router'
 
 const router = useRouter();
 
-const redirectDashboard = async () => {
- await router.push({name: 'dashboard'})
+const redirectDashboard = () => {
+  router.push({name: "dashboard"})
 }
 
-export default defineComponent({
-  name: 'IndexPage'
-})
 </script>
